4dbb866153001b0aa999049bf5b9bccb0e9f18cd
Daniel Mills <mills.dma@gmail.com>
2014-11-13 23:52:34 -0500

    example cleanup

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ng-nhl
 
-A small directive for loading nhl team's logos [demo](http://dmamills.github.io/ng-nhl)
+A angular directive for loading nhl team's logos [demo](http://dmamills.github.io/ng-nhl)
+
+#### you actually want to use this?
+
+```
+bower install ng-nhl
+```
--- demo.js
+++ demo.js
@@ -3,5 +3,5 @@ angular.module('app',['dmamills.ngNHL'])
 	  
 	  $scope.teams = ["Anaheim Ducks","Arizona Coyotes","Boston Bruins","Buffalo Sabres","Calgary Flames","Carolina Hurricanes","Chicago Blackhawks","Colorado Avalanche","Columbus Blue Jackets","Dallas Stars","Detroit Red Wings","Edmonton Oilers","Florida Panthers","Los Angeles Kings","Minnesota Wild","Montreal Canadiens","Nashville Predators","New Jersey Devils","New York Islanders","New York Rangers","Philadelphia Flyers","Pittsburgh Penguins","Ottawa Senators","San Jose Sharks","St. Louis Blues","Tampa Bay Lightning","Toronto Maple Leafs","Vancouver Canucks","Washington Capitals","Winnipeg Jets"];
 	  $scope.teamb = 'Anaheim Ducks';
-		$scope.teama = 'Detroit Red Wings';
+   	  $scope.teama = 'Detroit Red Wings';
 });
